476d5d6f0f3d7245a03b4583d1b030fa2c9cafbf
Dominik Madarász <zaklaus@outlook.com>
2021-10-06 18:42:33 +0200

    chore: fix remark

--- code/framework/src/launcher/data/tls.cpp
+++ code/framework/src/launcher/data/tls.cpp
@@ -2,7 +2,7 @@
  * This file is part of the CitizenFX project - http://citizen.re/
  *
  * See LICENSE and MENTIONS in the root of the source tree for information
-s * regarding licensing.
+ * regarding licensing.
  */
 
 #include <cstdint>
