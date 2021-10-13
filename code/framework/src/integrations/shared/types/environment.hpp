2c45e8aa55d5bd07ab97a90077bb0de39728866c
Dominik Madarász <zaklaus@outlook.com>
2021-10-13 14:56:23 +0200

    Ensure weather manager has a dummy transform so it can be picked up by the streamer

--- code/framework/src/integrations/shared/types/environment.hpp
+++ code/framework/src/integrations/shared/types/environment.hpp
@@ -29,6 +29,8 @@ namespace Framework::Integrations::Shared::Archetypes {
         inline flecs::entity CreateWeather(Args &&...args) {
             auto e = _world->entity<Args...>(std::forward<Args>(args)...);
 
+            e.add<World::Modules::Base::Transform>();
+
             auto weatherData = e.get_mut<Shared::Modules::Mod::Environment>();
             weatherData->timeHours = 12.0f;
 
