c42e361f8fd91861e682f2c3739b77a7010f4612
Vektor <chasterw@gmail.com>
2021-10-14 13:45:18 +0200

    visual studio filters

---
+++ scripts/get_all_targets.cmake
@@ -0,0 +1,17 @@
+# Function to get targets within a directory (and subdirectories)
+# https://newbedev.com/how-do-i-iterate-over-all-cmake-targets-programmatically
+function(get_all_targets var)
+    set(targets)
+    get_all_targets_recursive(targets ${CMAKE_CURRENT_SOURCE_DIR})
+    set(${var} ${targets} PARENT_SCOPE)
+endfunction()
+
+macro(get_all_targets_recursive targets dir)
+    get_property(subdirectories DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
+    foreach(subdir ${subdirectories})
+        get_all_targets_recursive(${targets} ${subdir})
+    endforeach()
+
+    get_property(current_targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
+    list(APPEND ${targets} ${current_targets})
+endmacro()
