e26c31f07ffad7294c7707294caaccfd49dae14c
Vektor <chasterw@gmail.com>
2021-10-15 11:28:20 +0200

    use serializer for packets

--- code/framework/src/networking/messages/client_connection_finalized.h
+++ code/framework/src/networking/messages/client_connection_finalized.h
@@ -18,7 +18,7 @@ namespace Framework::Networking::Messages {
         float _serverTickRate = 0.0f;
 
       public:
-        uint32_t GetMessageID() const override {
+        uint8_t GetMessageID() const override {
             return GAME_CONNECTION_FINALIZED;
         }
 
@@ -26,13 +26,8 @@ namespace Framework::Networking::Messages {
             _serverTickRate = tickRate;
         }
 
-        void FromBitStream(SLNet::BitStream *stream) override {
-            stream->Read(_serverTickRate);
-        }
-
-        SLNet::BitStream *ToBitStream(SLNet::BitStream *stream) override {
-            stream->Write(_serverTickRate);
-            return stream;
+        void Serialize(SLNet::BitStream *bs, bool write) override {
+            bs->Serialize(write, _serverTickRate);
         }
 
         bool Valid() override {
