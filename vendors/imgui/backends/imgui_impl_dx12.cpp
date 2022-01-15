58aea5750572f704884faddcdf7d363d4f81b86f
Dominik Madarász <zaklaus@outlook.com>
2022-01-15 02:13:51 +0100

    Fix up imgui on 32-bit systems + shadow drop support

--- vendors/imgui/backends/imgui_impl_dx12.cpp
+++ vendors/imgui/backends/imgui_impl_dx12.cpp
@@ -48,6 +48,12 @@
 #pragma comment(lib, "d3dcompiler") // Automatically link with d3dcompiler.lib as we are using D3DCompile() below.
 #endif
 
+#include <intrin.h>
+
+#ifndef _M_AMD64
+#define ImTextureID ImU64
+#endif
+
 // DirectX data
 struct ImGui_ImplDX12_RenderBuffers
 {
@@ -433,7 +439,12 @@ static void ImGui_ImplDX12_CreateFontsTexture()
     // [Solution 3] IDE/msbuild: edit imconfig.h and add '#define ImTextureID ImU64' (prefer solution 2 to create your own config file!)
     // [Solution 4] command-line: add '/D ImTextureID=ImU64' to your cl.exe command-line (this is what we do in the example_win32_direct12/build_win32.bat file)
     static_assert(sizeof(ImTextureID) >= sizeof(bd->hFontSrvGpuDescHandle.ptr), "Can't pack descriptor handle into TexID, 32-bit not supported yet.");
+
+#ifndef _M_AMD64
+    io.Fonts->SetTexID((ImTextureID*)bd->hFontSrvGpuDescHandle.ptr);
+#else
     io.Fonts->SetTexID((ImTextureID)bd->hFontSrvGpuDescHandle.ptr);
+#endif
 }
 
 bool    ImGui_ImplDX12_CreateDeviceObjects()
--- vendors/imgui/imconfig.h
+++ vendors/imgui/imconfig.h
@@ -122,3 +122,17 @@ namespace ImGui
     void MyFunction(const char* name, const MyMatrix44& v);
 }
 */
+
+namespace ImGui {
+    inline bool custom_UseFontShadow;
+    inline unsigned int custom_FontShadowColor;
+
+    inline static void PushFontShadow(unsigned int col) {
+        custom_UseFontShadow   = true;
+        custom_FontShadowColor = col;
+    }
+
+    inline static void PopFontShadow(void) {
+        custom_UseFontShadow = false;
+    }
+}; // namespace ImGui
