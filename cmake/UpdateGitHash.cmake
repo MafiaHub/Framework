6b6a456a1953c3d28d86901566f611e47ff10b8d
Dominik Madarász <zaklaus@outlook.com>
2022-01-17 17:52:28 +0100

    Fix the script so that it properly resolves macro defs

--- cmake/UpdateGitHash.cmake
+++ cmake/UpdateGitHash.cmake
@@ -8,11 +8,15 @@ macro(update_git_version mod_prefix in_file out_file)
         find_package(Git QUIET)
     endif()
 
+    set(TMP_GIT_VERSION ${${mod_prefix}_GIT_VERSION_LONG})
+    message(STATUS "Project: ${mod_prefix} SHA1: ${TMP_GIT_VERSION} build: ${${mod_prefix}_VERSION}")
+
     execute_process(
-        COMMAND "${GIT_EXECUTABLE}" rev-parse --short ${mod_prefix}_GIT_VERSION_LONG}
+        COMMAND "${GIT_EXECUTABLE}" rev-parse --short ${TMP_GIT_VERSION}
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         RESULT_VARIABLE res
         OUTPUT_VARIABLE ${mod_prefix}_GIT_VERSION
         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
     configure_file(${in_file} ${out_file} @ONLY)
 endmacro()
