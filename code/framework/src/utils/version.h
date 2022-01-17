6e9ea4da043f80581bb26472c607dc9be9a246ee
Dominik Madarász <zaklaus@outlook.com>
2022-01-17 16:33:37 +0100

    Add Git version info generator

---
+++ code/framework/src/utils/version.h
@@ -0,0 +1,14 @@
+/*
+ * MafiaHub OSS license
+ * Copyright (c) 2022, MafiaHub. All rights reserved.
+ *
+ * This file comes from MafiaHub, hosted at https://github.com/MafiaHub/Framework.
+ * See LICENSE file in the source repository for information regarding licensing.
+ */
+
+#pragma once
+
+namespace Framework::Utils::Version {
+    extern const char *gitLong;
+    extern const char *git;
+};
