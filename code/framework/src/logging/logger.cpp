b7e7ef30cd8bc3a2ab832d2c0e85460bcc1bf4c5
Dominik Madarász <zaklaus@outlook.com>
2022-01-20 16:10:12 +0100

    Tweak logger format
    Closes #65

--- code/framework/src/logging/logger.cpp
+++ code/framework/src/logging/logger.cpp
@@ -42,7 +42,7 @@ namespace Framework::Logging {
 
         auto consoleLogger = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
         consoleLogger->set_level(spdlog::level::debug);
-        consoleLogger->set_pattern("[%H:%M:%S] [%n] [%^%l%$] %v");
+        consoleLogger->set_pattern("[%H:%M:%S] [%^%l%$] [%n] %v");
 
         const auto fileLogName = _logFolder + "/" + _logName + ".log";
         auto fileLogger        = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(fileLogName, _maxFileSize, _maxFileCount);
