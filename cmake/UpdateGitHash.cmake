b19e2d1fd9239e239b775781f96c40b63808bef8
Dominik Madarász <zaklaus@outlook.com>
2022-01-22 16:59:49 +0100

    Remove extra check for Git package in update_git_version cmake module

--- cmake/UpdateGitHash.cmake
+++ cmake/UpdateGitHash.cmake
@@ -4,10 +4,6 @@ macro(update_git_version mod_prefix in_file out_file)
     GET_GIT_HEAD_REVISION(GIT_REFSPEC ${mod_prefix}_GIT_VERSION_LONG)
     file(STRINGS ${PROJECT_SOURCE_DIR}/VERSION ${mod_prefix}_VERSION)
 
-    if(NOT GIT_FOUND)
-        find_package(Git QUIET)
-    endif()
-
     set(TMP_GIT_VERSION ${${mod_prefix}_GIT_VERSION_LONG})
     message(STATUS "Project: ${mod_prefix} SHA1: ${TMP_GIT_VERSION} build: ${${mod_prefix}_VERSION}")
 
