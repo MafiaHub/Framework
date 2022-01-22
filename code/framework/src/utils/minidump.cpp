12247d39431b46b9de4e211fab4ee763a68e852b
Dominik Madarász <zaklaus@outlook.com>
2022-01-23 00:23:48 +0100

    Minidump: Add extra x64 common registers

--- code/framework/src/utils/minidump.cpp
+++ code/framework/src/utils/minidump.cpp
@@ -66,11 +66,21 @@ namespace Framework::Utils {
 		"RAX: {:<12x} RCX: {:<12x} \n"
 		"RDX: {:<12x} RBX: {:<12x} \n"
 		"RSP: {:<12x} RBP: {:<12x} \n"
-		"RSI: {:<12x} RDI: {:<12x} \n",
+		"RSI: {:<12x} RDI: {:<12x} \n"
+		"R8 : {:<12x} R9 : {:<12x} \n"
+		"R10: {:<12x} R11: {:<12x} \n"
+		"R12: {:<12x} R13: {:<12x} \n"
+		"R14: {:<12x} R15: {:<12x} \n",
 		(DWORD64)exceptionInfo->ExceptionRecord->ExceptionAddress,
-		exceptionInfo->ExceptionRecord->ExceptionCode, exceptionInfo->ContextRecord->Rax,
-		exceptionInfo->ContextRecord->Rcx, exceptionInfo->ContextRecord->Rdx, exceptionInfo->ContextRecord->Rbx,
-		exceptionInfo->ContextRecord->Rsp, exceptionInfo->ContextRecord->Rbp, exceptionInfo->ContextRecord->Rsi, exceptionInfo->ContextRecord->Rdi);
+		exceptionInfo->ExceptionRecord->ExceptionCode,
+        exceptionInfo->ContextRecord->Rax, exceptionInfo->ContextRecord->Rcx,
+        exceptionInfo->ContextRecord->Rdx, exceptionInfo->ContextRecord->Rbx,
+		exceptionInfo->ContextRecord->Rsp, exceptionInfo->ContextRecord->Rbp,
+        exceptionInfo->ContextRecord->Rsi, exceptionInfo->ContextRecord->Rdi,
+        exceptionInfo->ContextRecord->R8, exceptionInfo->ContextRecord->R9,
+        exceptionInfo->ContextRecord->R10, exceptionInfo->ContextRecord->R11,
+        exceptionInfo->ContextRecord->R12, exceptionInfo->ContextRecord->R13,
+        exceptionInfo->ContextRecord->R14, exceptionInfo->ContextRecord->R15);
 #elif _M_X86
         std::string crashInfo = fmt::format("{:X} with code: {:X} \n\nRegisters: \n"
 		"EAX: {:<12x} ECX: {:<12x} \n"
