2de1a07ef208bd32cfe1b112575419407728d778
Dominik Madarász <zaklaus@outlook.com>
2022-05-09 14:32:28 +0200

    net: describe messages.h

--- code/framework/src/networking/messages/messages.h
+++ code/framework/src/networking/messages/messages.h
@@ -14,24 +14,35 @@
 #include <function2.hpp>
 
 namespace Framework::Networking::Messages {
-    using MessageCallback          = fu2::function<void(SLNet::BitStream *) const>;
     using PacketCallback           = fu2::function<void(SLNet::Packet *) const>;
     using DisconnectPacketCallback = fu2::function<void(SLNet::Packet *, uint32_t reason) const>;
 
     enum DisconnectionReason { NO_FREE_SLOT, GRACEFUL_SHUTDOWN, LOST, FAILED, INVALID_PASSWORD, WRONG_VERSION, BANNED, KICKED, KICKED_INVALID_PACKET, UNKNOWN };
 
+    // Internal Framework messages
     enum InternalMessages : uint8_t { INTERNAL_RPC = ID_USER_PACKET_ENUM + 1, INTERNAL_NEXT_MESSAGE_ID };
+
+    // Internal game flow messages
     enum GameMessages : uint8_t {
+        // Game messages handling common client connection flow
         GAME_CONNECTION_HANDSHAKE = INTERNAL_NEXT_MESSAGE_ID,
         GAME_CONNECTION_FINALIZED,
         GAME_CONNECTION_KICKED,
+
+        // Game sync entity streamer messages
         GAME_SYNC_ENTITY_SPAWN,
         GAME_SYNC_ENTITY_UPDATE,
         GAME_SYNC_ENTITY_SELF_UPDATE, // server sends data to streamer
         GAME_SYNC_ENTITY_DESPAWN,
+
+        // Messages used by the mod
         GAME_NEXT_MESSAGE_ID
     };
 
+    /**
+     * Base interface for network message
+     * \see NetworkPeer::RegisterMessage
+     */
     class IMessage {
       private:
         SLNet::Packet *packet{};
