80c11bf233bbd8547a7c788726796f9636030399
Dominik Madarász <zaklaus@outlook.com>
2022-08-23 20:01:09 +0200

    fix cmake + copyright lint

--- code/framework/src/scripting/engines/resource.h
+++ code/framework/src/scripting/engines/resource.h
@@ -1,10 +1,10 @@
 /*
-* MafiaHub OSS license
-* Copyright (c) 2022, MafiaHub. All rights reserved.
-*
-* This file comes from MafiaHub, hosted at https://github.com/MafiaHub/Framework.
-* See LICENSE file in the source repository for information regarding licensing.
-*/
+ * MafiaHub OSS license
+ * Copyright (c) 2022, MafiaHub. All rights reserved.
+ *
+ * This file comes from MafiaHub, hosted at https://github.com/MafiaHub/Framework.
+ * See LICENSE file in the source repository for information regarding licensing.
+ */
 
 #pragma once
 
