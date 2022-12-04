aa52f163d0bbc94435cff36c957d7d942b278cb7
Segfault <5221072+Segfaultd@users.noreply.github.com>
2022-12-04 22:27:30 +0100

    First AI improvement

--- code/framework/src/scripting/engines/node/sdk.cpp
+++ code/framework/src/scripting/engines/node/sdk.cpp
@@ -20,23 +20,32 @@
 
 namespace Framework::Scripting::Engines::Node {
     static void On(const v8::FunctionCallbackInfo<v8::Value> &info) {
-        if (info.Length() == 2) {
-            if (!info[0]->IsString() || !info[1]->IsFunction()) {
-                return;
-            }
-
-            const auto isolate = info.GetIsolate();
-            const auto ctx     = isolate->GetEnteredOrMicrotaskContext();
-
-            v8::Local<v8::String> eventName       = info[0]->ToString(ctx).ToLocalChecked();
-            v8::Local<v8::Function> eventCallback = info[1].As<v8::Function>();
-
-            v8::Persistent<v8::Function> persistentCallback;
-            persistentCallback.Reset(isolate, eventCallback);
-
-            const auto resource = static_cast<Node::Resource *>(ctx->GetAlignedPointerFromEmbedderData(0));
-            resource->_eventHandlers[Helpers::ToString(eventName, isolate)].push_back(persistentCallback);
+        // Ensure that the method was called with exactly two arguments
+        if (info.Length() != 2) {
+            v8::Isolate* isolate = info.GetIsolate();
+            isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
+            return;
         }
+
+        // Ensure that the first argument is a string and the second is a function
+        if (!info[0]->IsString() || !info[1]->IsFunction()) {
+            // Throw an error if the argument types are incorrect
+            v8::Isolate* isolate = info.GetIsolate();
+            isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Invalid argument types: must be string and function").ToLocalChecked()));
+            return;
+        }
+
+        const auto isolate = info.GetIsolate();
+        const auto ctx     = isolate->GetEnteredOrMicrotaskContext();
+
+        v8::Local<v8::String> eventName       = info[0]->ToString(ctx).ToLocalChecked();
+        v8::Local<v8::Function> eventCallback = info[1].As<v8::Function>();
+
+        // Create a persistent handle to the event callback function
+        v8::Persistent<v8::Function> persistentCallback(isolate, eventCallback);
+
+        const auto resource = static_cast<Node::Resource *>(ctx->GetAlignedPointerFromEmbedderData(0));
+        resource->_eventHandlers[Helpers::ToString(eventName, isolate)].push_back(persistentCallback);
     }
 
     bool SDK::Init(v8::Isolate *isolate, SDKRegisterCallback cb) {
