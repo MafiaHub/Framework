56ef69b20d57dff0a7680bf3967992b70e05767e
Dominik Madarász <zaklaus@outlook.com>
2022-12-06 15:22:45 +0100

    update release script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -3,10 +3,10 @@
 set -xe
 
 git fetch origin
-git checkout master
-git reset --hard master
+git checkout release
+git reset --hard release
 git merge develop --squash --allow-unrelated-histories -X theirs
 git commit
-git push origin master
+git push origin release
 git checkout develop
 echo Done!
