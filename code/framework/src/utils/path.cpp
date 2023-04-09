03e8de2846528e89a2733b3db5bf81621117dab7
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-04-09 19:34:37 +0200

    Added path utils

---
+++ code/framework/src/utils/path.cpp
@@ -0,0 +1,41 @@
+#include "safe_win32.h"
+#include <Shlwapi.h>
+#include "path.h"
+
+namespace Framework::Utils {
+    std::wstring GetAbsolutePathW(const std::wstring &relative) {
+        static wchar_t executable_path[MAX_PATH] = {'\0'};
+
+        if (executable_path[0] == '\0') {
+            wchar_t buf[MAX_PATH];
+            GetModuleFileNameW(nullptr, buf, MAX_PATH);
+            _wsplitpath(buf, &executable_path[0], &executable_path[_MAX_DRIVE - 1], nullptr, nullptr);
+        }
+
+        wchar_t buf[MAX_PATH];
+        lstrcpyW(buf, executable_path);
+        lstrcatW(buf, relative.c_str());
+
+        wchar_t final_buf[MAX_PATH] = {'\0'};
+        PathCanonicalizeW(final_buf, buf);
+        return final_buf;
+    }
+
+    std::string GetAbsolutePathA(const std::string &relative) {
+        static char executable_path[MAX_PATH] = {'\0'};
+
+        if (executable_path[0] == '\0') {
+            char buf[MAX_PATH];
+            GetModuleFileNameA(nullptr, buf, MAX_PATH);
+            _splitpath(buf, &executable_path[0], &executable_path[_MAX_DRIVE - 1], nullptr, nullptr);
+        }
+
+        char buf[MAX_PATH];
+        strcpy(buf, executable_path);
+        strcat(buf, relative.c_str());
+
+        char final_buf[MAX_PATH] = {'\0'};
+        PathCanonicalizeA(final_buf, buf);
+        return final_buf;
+    }
+}
---
+++ code/framework/src/utils/path.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+
+namespace Framework::Utils {
+    std::wstring GetAbsolutePathW(const std::wstring &);
+    std::string GetAbsolutePathA(const std::string &);
+}
