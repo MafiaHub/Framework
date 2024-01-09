3fcd49937b6828c470525119cacff02e3df9ad12
Dominik Madarász <zaklaus@outlook.com>
2024-01-09 15:35:00 +0100

    fix scripting engine UTs

--- code/tests/modules/scripting_engine_ut.h
+++ code/tests/modules/scripting_engine_ut.h
@@ -14,23 +14,6 @@
 MODULE(scripting_engine, {
     using namespace Framework::Scripting;
 
-    IT("can allocate and deallocate a valid NodeJS scripting engine instance", {
-        Module *pEngine = new Module;
-
-        // Init the engine and make sure everything went fine
-        EQUALS(pEngine->Init(EngineTypes::ENGINE_NODE, NULL), ModuleError::MODULE_NONE);
-        NEQUALS(pEngine->GetEngine(), nullptr);
-        NEQUALS(reinterpret_cast<Engines::Node::Engine *>(pEngine->GetEngine())->GetIsolate(), nullptr);
-        NEQUALS(reinterpret_cast<Engines::Node::Engine *>(pEngine->GetEngine())->GetPlatform(), nullptr);
-
-        // Shutdown the engine and make sure everything went down
-        EQUALS(pEngine->Shutdown(), ModuleError::MODULE_NONE);
-        EQUALS(reinterpret_cast<Engines::Node::Engine *>(pEngine->GetEngine())->GetIsolate(), nullptr);
-        EQUALS(reinterpret_cast<Engines::Node::Engine *>(pEngine->GetEngine())->GetPlatform(), nullptr);
-
-        delete pEngine;
-    });
-
     IT("can allocate and deallocate a valid scripting engine instance, then do it again to test re-entry", {
         Module *pEngine = new Module;
 
@@ -48,6 +31,7 @@ MODULE(scripting_engine, {
         delete pEngine;
 
         // Intentionally duplicated to test engine for re-entry
+        pEngine = new Module;
 
         // Init the engine and make sure everything went fine
         EQUALS(pEngine->Init(EngineTypes::ENGINE_NODE, NULL), ModuleError::MODULE_NONE);
