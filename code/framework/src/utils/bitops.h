df5ec1a98b6f54e463fed8668bab48f8873abc92
Dominik Madarász <zaklaus@outlook.com>
2024-01-11 18:50:04 +0100

    utils: add bitops.h

---
+++ code/framework/src/utils/bitops.h
@@ -0,0 +1,33 @@
+/*
+ * MafiaHub OSS license
+ * Copyright (c) 2021-2023, MafiaHub. All rights reserved.
+ *
+ * This file comes from MafiaHub, hosted at https://github.com/MafiaHub/Framework.
+ * See LICENSE file in the source repository for information regarding licensing.
+ */
+
+#pragma once
+
+// Define a flag at given position 'pos' in enum
+#define BIT_FLD(pos) (1U << (pos))
+
+// Set the bit at position 'pos' in 'var'
+#define BIT_SET(var, pos) ((var) |= BIT_FLD(pos))
+
+// Clear the bit at position 'pos' in 'var'
+#define BIT_CLR(var, pos) ((var) &= ~BIT_FLD(pos))
+
+// Check if the bit at position 'pos' in 'var' is set
+#define BIT_HAS(var, pos) ((var)&BIT_FLD(pos))
+
+// Perform bitwise NOT on 'var'
+#define BIT_NOT(var) (~(var))
+
+// Perform bitwise OR between 'var' and 'mask'
+#define BIT_OR(var, mask) ((var) |= (mask))
+
+// Perform bitwise XOR between 'var' and 'mask'
+#define BIT_XOR(var, mask) ((var) ^= (mask))
+
+// Perform bitwise AND between 'var' and 'mask'
+#define BIT_AND(var, mask) ((var) &= (mask))
