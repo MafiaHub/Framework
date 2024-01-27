231d90e795f06d8ebb63b10567a4aaac36f1baa8
Dominik Madarász <zaklaus@outlook.com>
2024-01-27 13:43:28 +0100

    the rest of the changes, yeppers

--- code/framework/src/services/masterlist/masterlist.cpp
+++
@@ -1,7 +0,0 @@
-#include "masterlist.h"
-
-namespace Framework::Services {
-    MasterlistBase::MasterlistBase() {
-        _client = std::make_shared<httplib::Client>("https://api.mafiahub.dev");
-    }
-}
