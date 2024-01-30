6cadb3ac31dc3b2981066d8e16c7df9068b8f011
Dominik Madarász <zaklaus@outlook.com>
2024-01-30 18:08:40 +0100

    fix cpu usage

--- code/framework/src/services/masterlist.cpp
+++ code/framework/src/services/masterlist.cpp
@@ -36,9 +36,10 @@ namespace Framework::Services {
     void MasterlistConnector::PingThread() {
         while (_isInitialized) {
             std::lock_guard lock(_mutex);
-            
+
             // Only ping every 5 seconds
             if (Utils::Time::GetDifference(Utils::Time::GetTimePoint(), _lastPingAt) < 5000) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 continue;
             }
 
