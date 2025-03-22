dd4034d5a9847bb13db1acc627ce5277be7211be
Dominik Madarász <zaklaus@outlook.com>
2025-03-22 12:08:11 +0100

    expose base events for mod-level modifications [low-level use cases]

--- code/framework/src/world/modules/base.hpp
+++ code/framework/src/world/modules/base.hpp
@@ -127,11 +127,11 @@ namespace Framework::World::Modules {
             Events events;
 
           public:
-            Events GetBaseEvents() const {
+            Events& GetBaseEvents() {
                 return events;
             }
 
-            [[maybe_unused]] Events GetModEvents() const {
+            [[maybe_unused]] Events& GetModEvents() {
                 return modEvents;
             }
         };
