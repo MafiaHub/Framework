152b9875c76e4355dc82163911325ce44a02ebc5
Sebastian Jura <22455534+CrosRoad95@users.noreply.github.com>
2025-08-03 07:27:53 +0200

    Add logging formatters

---
+++ code/framework/src/logging/formatters.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "src/world/modules/base.hpp"
+#include <spdlog/fmt/bundled/format.h>
+
+template <>
+struct fmt::formatter<glm::vec3> {
+    constexpr auto parse(format_parse_context &ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const glm::vec3 &t, FormatContext &ctx) {
+        return format_to(ctx.out(), "{:.2f}, {:.2f}, {:.2f}", t.x, t.y, t.z);
+    }
+};
+
+template <>
+struct fmt::formatter<glm::quat> {
+    constexpr auto parse(format_parse_context &ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const glm::quat &t, FormatContext &ctx) {
+        return format_to(ctx.out(), "{:.2f}, {:.2f}, {:.2f}, {:.2f}", t.x, t.y, t.z, t.w);
+    }
+};
+
+template <>
+struct fmt::formatter<Framework::World::Modules::Base::Transform> {
+    constexpr auto parse(format_parse_context &ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const Framework::World::Modules::Base::Transform &t, FormatContext &ctx) {
+        return format_to(ctx.out(), "Transform(pos=({}), vel=({}), rot=({}), genID={})", t.pos, t.vel, t.rot, t.GetGeneration());
+    }
+};
