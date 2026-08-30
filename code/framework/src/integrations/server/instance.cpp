cec003df26b58046fedb7c4b25cc1a537a45b013
Kheartz <kheartztv@gmail.com>
2026-08-30 05:26:31 -0700

    Server: Report the mod's own version on the info endpoint (#262)
    * Server: Report the mod's own version on the info endpoint
    
    mod_version carried Framework::Utils::Version::rel, so the endpoint
    advertised the framework's version under the mod's name. Use
    _opts.modVersion and expose the framework build as framework_version.
    
    * Server: Announce the mod's version to the masterlist
    
    The heartbeat carried the framework's version, so server browsers listed
    a framework build number where players expect the mod release they
    downloaded. The build token still pairs both versions separately.

--- code/framework/src/integrations/server/instance.cpp
+++ code/framework/src/integrations/server/instance.cpp
@@ -288,7 +288,8 @@ namespace Framework::Integrations::Server {
             nlohmann::json root;
             root["mod_name"]          = _opts.modName;
             root["mod_slug"]          = _opts.modSlug;
-            root["mod_version"]       = Utils::Version::rel;
+            root["mod_version"]       = _opts.modVersion;
+            root["framework_version"] = Utils::Version::rel;
             root["host"]              = _opts.bindHost;
             root["port"]              = _opts.bindPort;
             root["password_required"] = !_opts.bindPassword.empty();
@@ -1071,7 +1072,7 @@ namespace Framework::Integrations::Server {
                 Services::ServerInfo info {};
                 info.port           = _opts.bindPort;
                 info.gameMode       = _opts.modName;
-                info.version        = Utils::Version::rel;
+                info.version        = _opts.modVersion;
                 info.maxPlayers     = _opts.maxPlayers;
                 info.currentPlayers = _networkingEngine->GetNetworkServer()->GetPeer()->NumberOfConnections();
                 _masterlist->Ping(info);
